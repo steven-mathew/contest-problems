a52d9f484be8b07b1c767afc6dcce3f908585adf
Steven Mathew <40929644+indra963@users.noreply.github.com>
2018-12-21 15:57:34 -0500

    Update HashtableCheck.java

--- HashtableCheck.java
+++ HashtableCheck.java
@@ -2,26 +2,21 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class HashtableCheck {
-
 	public static char firstRepeat(String str) {
-
 		HashSet<Character> h = new HashSet<>();
 
 		for(char ch : str.toCharArray()) {
-
 			if (h.contains(ch)) {
 				return ch;
 			} else {
 				h.add(ch);
 			}			
 		}
-
+		
 		return 0;
-
 	}	
 
 	public static void main(String[] args) {
-
 		String str = null;
 
 		Scanner sc = new Scanner(System.in);
@@ -34,10 +29,8 @@ public class HashtableCheck {
 
 		} catch(Exception e) {
 			e.printStackTrace();
-		}
-		
+		}	
 	}
-
 }
 
 
