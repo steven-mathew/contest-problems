21b7de95fbc4f99b782fe51d223d9cce7cb5318c
Steven Mathew <40929644+indra963@users.noreply.github.com>
2018-12-25 18:11:49 -0500

    Update System0.java

--- System0.java
+++ System0.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class System0 {
-	public static void main(String... args) {
-		
+	public static void main(String... args) {	
 		Scanner sc = new Scanner(System.in);		
 		int n = Integer.parseInt(sc.nextLine());
 		
