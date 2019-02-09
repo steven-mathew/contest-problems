36dedf2e6fdc3b5ad82dd1af865e116713e5fdab
Steven Mathew <40929644+steven-mathew@users.noreply.github.com>
2019-02-08 23:44:57 -0500

    Update Avalon.java

--- BTS/Avalon.java
+++ BTS/Avalon.java
@@ -3,9 +3,7 @@ package BTS;
 import java.util.Scanner;
 
 public class Avalon {
-
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		
 		int n = Integer.parseInt(sc.nextLine());
