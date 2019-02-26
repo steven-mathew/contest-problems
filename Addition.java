3cd48f45c8f7367141f789c176d236de8d38eb8e
Steven Mathew <40929644+steven-mathew@users.noreply.github.com>
2019-02-26 18:29:17 -0500

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -6,8 +6,7 @@ public class Addition {
 		String str = null;
 		int n1 = 0;
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));				
 		try {
 			str = br.readLine();
 			String[] tokens = str.split(" -> ");
@@ -15,7 +14,6 @@ public class Addition {
 			for(int i = tokens.length - 1; i >= 0; i--) {				
 				n1 += Integer.parseInt(tokens[i])*Math.pow(10, i);		
 			}
-
 			System.out.println(n1);
 		} catch(Exception e) {
 			e.printStackTrace();
