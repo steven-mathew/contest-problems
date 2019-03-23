323838d13d1ad00361c032708b4604c3d6cc8e4d
Steven Mathew <40929644+steven-mathew@users.noreply.github.com>
2019-03-23 17:57:06 -0400

    Update ECOOReading.java

--- ECOOReading.java
+++ ECOOReading.java
@@ -17,15 +17,7 @@ public class ECOOReading {
 
 	public static void main(String[] args) throws IOException {
 
-		int[][] arr = new int[5][5];
 		
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				arr[i][j] = new Random().nextInt(5+1);
-				System.out.print(arr[i][j]);
-			}
-			System.out.println();
-		}
 				
 		System.exit(0);
 	}
