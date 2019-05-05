05b9bb7c4536e57c8941ba2d0d084ea0b750b89b
Steven Mathew <40929644+steven-mathew@users.noreply.github.com>
2019-05-05 18:28:34 -0400

    Update ReverseWords.java

--- ReverseWords.java
+++ ReverseWords.java
@@ -1,27 +1,19 @@
 import java.util.Scanner;
 
 public class ReverseWords {
-
 	public static void main(String[] args) {
-
 		String str = null;
 
 		try {
-
 			Scanner sc = new Scanner(System.in);
 			str = sc.nextLine();	
 			String[] word = str.split(" ");
 			
 			for(int i = word.length - 1; i >= 0; i--) {
-				
-				System.out.print(word[i] + " ");
-				
+				System.out.print(word[i] + " ");	
 			}
-		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-
 	}
-
 }
